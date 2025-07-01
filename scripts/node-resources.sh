0fece64047a3405e0a2781157346d4de4d7c0ba6
Jesse Austin <jessedaustin@gmail.com>
2025-06-30 21:01:10 -0400

    Add node resources

--- scripts/node-resources.sh
+++ scripts/node-resources.sh
@@ -3,9 +3,9 @@
 LOG=~/mesh-admin/node-resources.log
 
 while read NODE; do
-    echo "===== $NODE =====" | tee -a $LOG
-    ssh $NODE 'echo CPU:; lscpu | grep "Model name"; echo RAM:; free -h | grep Mem; echo Disk:; df -h /; uptime' 2>&1 | tee -a $LOG
-    echo "" | tee -a $LOG
+    NODE=$(echo "$NODE" | cut -d'#' -f1 | xargs)
+    [ -z "$NODE" ] && continue
+    # your code...
 done < ~/mesh-admin/nodelist.txt
 
 echo "Resource check complete. See $LOG"
